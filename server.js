36b17191f7266d36fbe8623eefcbc2a07855260f
coders-angular <angular.coders@gmail.com>
2022-08-27 15:46:02 +0530

    server changes

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/protofolioapp'));
+app.use(express.static('./dist/angular-protofolio'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/protofolioapp/'}),
+    res.sendFile('index.html', {root: 'dist/angular-protofolio/'}),
 );
 
 // Start the app by listening on the default Heroku port
