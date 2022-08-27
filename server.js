a08bbbafdfaf401a1616f8805a6e4479b63e573b
coders-angular <angular.coders@gmail.com>
2022-08-27 22:38:18 +0530

    server changes

--- server.js
+++ server.js
@@ -6,11 +6,8 @@ app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
 
 //PATH LOCATION STARTEGY
-
 app.get('/*', function(req,res){
   const fullPath = path.join(__dirname + '/dist/index.html');
   console.log(" Fetching from.." + fullPath);
     res.sendFile(fullPath);
 })
-
-console.log('Server started running..');
